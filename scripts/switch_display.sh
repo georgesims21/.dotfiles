af846ec4249680711a655c02bbc399d3414f7c71
George <georgesims21@hotmail.com>
2021-01-25 15:03:18 +0100

    Daily commit: 01/25/21 15:03:18

--- scripts/switch_display.sh
+++ scripts/switch_display.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 intern=eDP1
 extern=DP1
-background=$HOME/Pictures/wallpapers/mountain-wallpaper.jpg
+lidstate=false
 
-if xrandr | grep "current 3840"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
+# Check if the lid is open, if yes lidstate is set to true
+if [[ $(cat /proc/acpi/button/lid/LID0/state | awk '{print ($2)}') == "open" ]]; then
+    lidstate=true
+fi
+
+# If lid is open, put to right of external, closed turn it off
+if "$lidstate"; then
+    xrandr --output "$extern" --auto --output "$intern" --auto --right-of "$extern"
 else
     xrandr --output "$intern" --off --output "$extern" --auto
 fi
-nitrogen --set-scaled "$background"
