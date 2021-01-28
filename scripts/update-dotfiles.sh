b01c96ee52b09e5feb82e2a9400abec91048c0d5
George <georgesims21@hotmail.com>
2021-01-28 18:24:23 +0100

    Daily commit: 01/28/21 18:24:23

--- scripts/update-dotfiles.sh
+++ scripts/update-dotfiles.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-LOG=/home/george/log.txt
-echo "Starting script" >> "$LOG"
-
 DOTS="$HOME"/github/.dotfiles
 TIME=$(date +"%D %T")
 
 git pull
 cd "$DOTS"
 git add *
-echo "Added everything" >> "$LOG"
 git commit -m "Daily commit: $TIME"
-echo "Committed everything" >> "$LOG"
 git push
-echo "Pushed everything" >> "$LOG"
