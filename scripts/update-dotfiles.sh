7b451b0d6d4cfec708f953020bb64154582859ca
George Sims <g.sims@m2mobi.com>
2022-03-30 20:37:38 +0200

    Daily commit: 03/30/22 20:37:38

--- scripts/update-dotfiles.sh
+++ scripts/update-dotfiles.sh
@@ -5,7 +5,7 @@ TIME=$(date +"%D %T")
 LOG=/var/log/scripts/update_dotfiles.log
 
 cd "$DOTS"
-echo "$TIME\n" >> "$LOG"
-echo "$(git add -A)\n" >> "$LOG"
-echo "$(git commit -m "Daily commit: $TIME")\n" >> "$LOG"
-echo "$(git push)\n" >> "$LOG"
+echo "$TIME" >> "$LOG"
+echo "$(git add -A)" >> "$LOG"
+echo "$(git commit -m "Daily commit: $TIME")" >> "$LOG"
+echo "$(git push)" >> "$LOG"
