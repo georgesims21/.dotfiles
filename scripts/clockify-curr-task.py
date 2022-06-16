fd28ecd56b770bfb4e7de404fcdf5d671e1fa447
George Sims <george.sims.nl@gmail.com>
2022-06-16 18:42:01 +0200

    Daily commit: 06/16/22 18:42:01

--- scripts/clockify-curr-task.py
+++ scripts/clockify-curr-task.py
@@ -2,12 +2,16 @@
 
 import subprocess
 import csv
+import sys
+import time
 
 encoding = 'ascii'    # specify the encoding of the CSV data
-p2 = subprocess.Popen(["clockify-cli", "show", "-v"], stdout=subprocess.PIPE)
+p2 = subprocess.Popen(["clockify-cli", "show", "-v"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 output = p2.communicate()[0].decode(encoding)
 reader = csv.DictReader(output.splitlines(), delimiter=",")
 for row in reader:
     print('(' + row['project.name'] + ')',
         row['description'],
         "[" + row['duration'] + "]")
+print("NOT RECORDING TIME!")
